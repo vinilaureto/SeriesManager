939701c6fc0ee4fb3f91c058d2fc46ecca59f7c4
Vinícius Laureto <vinilaureto@gmail.com>
2021-11-08 12:15:41 -0300

    Create series controller

---
+++ app/src/main/java/com/vinilaureto/seriesmanager/controllers/SeriesController.kt
@@ -0,0 +1,14 @@
+package com.vinilaureto.seriesmanager.controllers
+
+import com.vinilaureto.seriesmanager.MainActivity
+import com.vinilaureto.seriesmanager.database.Database
+import com.vinilaureto.seriesmanager.entities.series.Series
+
+class SeriesController(mainActivity: MainActivity) {
+    private val database = Database(mainActivity)
+
+    fun newSeries(series: Series) = database.createSeries(series)
+    fun findAllSeries() = database.findAllSeries()
+    fun updateSeries(series: Series) = database.updateSeries(series)
+    fun removeSeries(series: Series) = database.removeSeries(series)
+}
