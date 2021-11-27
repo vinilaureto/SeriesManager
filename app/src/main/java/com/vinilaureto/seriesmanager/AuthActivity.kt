5249d8e11138a61b6d35ee4d60f39279b3e35909
Vinícius Laureto <vinilaureto@gmail.com>
2021-11-27 20:49:07 -0300

    Add firebase dependencies

---
+++ app/src/main/java/com/vinilaureto/seriesmanager/AuthActivity.kt
@@ -0,0 +1,11 @@
+package com.vinilaureto.seriesmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
---
+++ app/src/main/java/com/vinilaureto/seriesmanager/RecoveryPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.vinilaureto.seriesmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecoveryPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery_password)
+    }
+}
