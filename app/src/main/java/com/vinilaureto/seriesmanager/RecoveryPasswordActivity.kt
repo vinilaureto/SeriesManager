00862dccffc14f94e17ec96e8e1a4fff44779be1
Vinícius Laureto <vinilaureto@gmail.com>
2021-11-27 22:07:04 -0300

    Recovery password activity implementation

--- app/src/main/java/com/vinilaureto/seriesmanager/RecoveryPasswordActivity.kt
+++ app/src/main/java/com/vinilaureto/seriesmanager/RecoveryPasswordActivity.kt
@@ -2,6 +2,8 @@ package com.vinilaureto.seriesmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.vinilaureto.seriesmanager.auth.AuthFirebase
 import com.vinilaureto.seriesmanager.databinding.ActivityAuthBinding
 import com.vinilaureto.seriesmanager.databinding.ActivityRecoveryPasswordBinding
 
@@ -13,5 +15,21 @@ class RecoveryPasswordActivity : AppCompatActivity() {
         activityRecoveryPasswordBinding = ActivityRecoveryPasswordBinding.inflate(layoutInflater)
         setContentView(activityRecoveryPasswordBinding.root)
         supportActionBar?.subtitle = "Recuperar senha"
+
+        with(activityRecoveryPasswordBinding) {
+            recoveryPasswordBt.setOnClickListener {
+                val email = emailEt.text.toString()
+                if (email.isNotEmpty()) {
+                    AuthFirebase.firebaseAuth.sendPasswordResetEmail(email).addOnSuccessListener {
+                        Toast.makeText(this@RecoveryPasswordActivity, "E-mail de recuperação enviado com sucesso!", Toast.LENGTH_LONG).show()
+                        finish()
+                    }.addOnFailureListener {
+                        Toast.makeText(this@RecoveryPasswordActivity, "Ops! Algum problema aconteceu", Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    Toast.makeText(this@RecoveryPasswordActivity, "O campo de e-mail não pode ser vazio", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
